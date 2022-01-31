2b7d240972674d7795d179ca95e095d637d9daa6
gschiess <sch19005@byui.edu>
2022-01-30 19:25:33 -0700

    changes

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const port = 3000;
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
     res.send("Hello browser");
 });
 
 app.post('/login', (req,res) =>{
-    if(req.body.userName =="gabrielschiess"){
+    console.log(JSON.stringify(req.body));
+    if(req.body.userName =="gabrielschiess" && req.body.password =="password"){
         res.send("Welcome!")
     } else{
         res.send("Get Thee Hence!");
