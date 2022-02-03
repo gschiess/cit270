2f2335934eb580e656370dd8409f101943975908
gschiess <sch19005@byui.edu>
2022-02-03 16:30:18 -0700

    adding stuff

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/',(req,res)=>{
 
 app.post('/login', (req,res) =>{
     console.log(JSON.stringify(req.body));
-    if(req.body.userName =="gabrielschiess" && req.body.password =="password"){
+    if(req.body.userName == "gabrielschiess" && md5(req.body.password) == '5f4dcc3b5aa765d61d8327deb882cf99'){
         res.send("Welcome!")
     } else{
         res.send("Get Thee Hence!");
