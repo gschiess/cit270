15c983788b95bde96464870fb44b0d4664ef1854
gschiess <sch19005@byui.edu>
2022-02-04 13:56:25 -0700

    it now works!

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const port = 3000;
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const md5 = require("md5");
 const app = express();
+const port = 3000;
+const fs = require('fs')
 
 app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
-    res.send("Hello browser");
+app.get("/", (req, res) => {
+  res.send("Hello HTTPS!");
 });
 
-app.post('/login', (req,res) =>{
-    console.log(JSON.stringify(req.body));
-    if(req.body.userName == "gabrielschiess" && md5(req.body.password) == '5f4dcc3b5aa765d61d8327deb882cf99'){
-        res.send("Welcome!")
-    } else{
-        res.send("Get Thee Hence!");
-    }
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert"),
+    },
+    app
+  )
+  .listen(3000, () => {
+    console.log("Listening...");
+  });
 
-app.listen(port, ()=>{})
+app.post("/login", (req, res) => {
+  console.log(JSON.stringify(req.body));
+  if (
+    req.body.userName == "gabrielschiess" &&
+    md5(req.body.password) == "5f4dcc3b5aa765d61d8327deb882cf99"
+  ) {
+    res.send("Welcome!");
+  } else {
+    res.send("Get Thee Hence!");
+  }
+});
